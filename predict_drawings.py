2c501c213794fd81855ca7cc0049a16599087282
ombakhshi <ombakh28@gmail.com>
2024-07-18 19:58:00 -0400

    draw_circle created

--- predict_drawings.py
+++ predict_drawings.py
@@ -5,3 +5,25 @@ import cv2
 
 # Load trained model
 model = tf.keras.models.load_model('model.h5')
+
+
+#Create canvas
+image = np.ones((400, 400), np.uint8) * 255
+drawing = False # set to true on mouse press
+
+ix, iy = -1, -1
+# mouse callback
+def draw_circle(event,x,y,flags,param):
+    global ix, iy, drawing
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix, iy = x, y
+
+    elif event == cv2.EVENT_MOUSEDOWN:
+        if drawing == True:
+            cv2.line(image, (ix, iy), (x, y), (0, 0, 0), 4)
+            ix, iy = x, y
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+        cv2.line(image, (ix, iy), (x, y), (0, 0, 0), 4)
