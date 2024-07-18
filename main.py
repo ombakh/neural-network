cd35a6f9efcce88badc69c6148e1fbef03275ec5
ombakhshi <ombakh28@gmail.com>
2024-07-18 19:31:35 -0400

    Layers

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 from tensorflow import keras
 
-
 fashiondata = tf.keras.datasets.mnist # Loading DataSet
 (x_train, y_train), (x_test, y_test) = fashiondata.load_data()
 
@@ -10,3 +9,11 @@ x_train.shape
 
 # Preprocessing
 x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Layers
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
