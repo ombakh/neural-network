afafa0311e3860e9bce6167073491e55492fd391
ombakhshi <ombakh28@gmail.com>
2024-07-18 20:15:20 -0400

    Works but is bad at predicting numbers properly.

--- predict_drawings.py
+++ predict_drawings.py
@@ -20,10 +20,37 @@ def draw_circle(event,x,y,flags,param):
         drawing = True
         ix, iy = x, y
 
-    elif event == cv2.EVENT_MOUSEDOWN:
+    elif event == cv2.EVENT_MOUSEMOVE:
         if drawing == True:
             cv2.line(image, (ix, iy), (x, y), (0, 0, 0), 4)
             ix, iy = x, y
     elif event == cv2.EVENT_LBUTTONUP:
         drawing = False
         cv2.line(image, (ix, iy), (x, y), (0, 0, 0), 4)
+
+cv2.namedWindow('draw')
+cv2.setMouseCallback('draw', draw_circle)
+
+# main While Loop
+
+while True:
+    cv2.imshow('draw', image)
+    k = cv2.waitKey(1) & 0xFF
+    if k == ord('c'):
+        image = np.ones((400, 400), np.uint8) * 255
+    elif k == ord('q'):
+        break
+    elif k == ord('p'):
+        resized = cv2.resize(image, (28, 28))
+
+        resized = cv2.bitwise_not(resized)
+
+        resized = resized / 255.0
+
+        input_image = resized.reshape(1, 28, 28)
+
+        prediction = model.predict(input_image)
+        predicted_digit = np.argmax(prediction)
+        print(f"PREDICTION: {predicted_digit}")
+
+cv2.destroyAllWindows()
